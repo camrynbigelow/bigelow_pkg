fa8e208f3e13dd43981fc2cdf96bd3b76576e833
Camryn Bigelow <camryn.bigelow@selu.edu>
2023-10-12 16:46:20 +0000

    delete copy

--- Setup notes.R
+++ Setup notes.R
@@ -11,8 +11,13 @@ git config --global user.email "camryn.bigelow@selu.edu"
 
 #R:
   use_git()
+<<<<<<< HEAD
 #whatever the positive option is
 create_github_token("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")
 gitcreds::gitcreds_set()
+=======
+3 whatever the positive option is
+
+>>>>>>> a651df6a321378757a8ecfaffe00a5043ba97ddc
 
 git_sitrep()
