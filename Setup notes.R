7b2c2f73b808922c01fbd90835f6903eee74cc8e
Camryn Bigelow <camryn.bigelow@selu.edu>
2023-10-12 16:26:40 +0000

    Initial commit

---
+++ Setup notes.R
@@ -0,0 +1,19 @@
+install.packages("usethis")
+library(usethis)
+
+install.packages("tidyverse")
+library(tidyverse)
+
+Terminal:
+  git config --global user.name "Camryn Bigelow"
+git config --global user.email "camryn.bigelow@selu.edu"
+
+
+R:
+  use_git()
+3 whatever the positive option is
+create_github_token("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")
+gitcreds::gitcreds_set("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")
+
+  
+}
