a651df6a321378757a8ecfaffe00a5043ba97ddc
camrynbigelow <146132217+camrynbigelow@users.noreply.github.com>
2023-10-12 11:33:39 -0500

    Update Setup notes.R

--- Setup notes.R
+++ Setup notes.R
@@ -12,8 +12,7 @@ git config --global user.email "camryn.bigelow@selu.edu"
 R:
   use_git()
 3 whatever the positive option is
-create_github_token("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")
-gitcreds::gitcreds_set("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")
+
 
   
 }
